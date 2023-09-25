a0aa801ae4b80d31c7336f63858fff655029eb32
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-25 10:43:53 +0800

    add sign to unipass

--- src/components/unipass.jsx
+++ src/components/unipass.jsx
@@ -1,42 +1,77 @@
 import { UniPassPopupSDK } from "@unipasswallet/popup-sdk";
-import {useState} from "react";
+import { useState } from "react";
 
-export  default function Unipass(){
+export default function Unipass() {
+  const [account, setAccount] = useState();
+  const [sig, setSig] = useState("");
 
-    const [account, setAccount] = useState();
-
-    const upWallet = new UniPassPopupSDK({
-        env: "test",
-        // for polygon mumbai
-        chainType: "polygon",
-        // choose localStorage if you want to cache user account permanent
-        storageType: "sessionStorage",
-        appSettings: {
-            // theme: UniPassTheme.LIGHT,
-            appName: "UniPass Wallet Demo",
-            appIcon: "",
+  const upWallet = new UniPassPopupSDK({
+    env: "test",
+    // for polygon mumbai
+    chainType: "polygon",
+    // choose localStorage if you want to cache user account permanent
+    storageType: "sessionStorage",
+    appSettings: {
+      // theme: UniPassTheme.LIGHT,
+      appName: "UniPass Wallet Demo",
+      appIcon: "",
+    },
+  });
+  const connect = async () => {
+    try {
+      const account = await upWallet.login({
+        email: true,
+        eventListener: (event) => {
+          console.log("event", event);
+          const { type, body } = event;
+          console.log("account", body);
         },
-    });
-    const connect = async() =>{
-        try {
-            const account = await upWallet.login({
-                email: true,
-                eventListener: (event) => {
-                    console.log("event", event);
-                    const { type, body } = event;
-                    console.log("account", body);
-                },
-                connectType: "both",
-            });
-            const { address, email } = account;
-            setAccount(address)
-            console.log("account", address, email);
-        } catch (err) {
-            console.log("connect err", err);
-        }
+        connectType: "both",
+      });
+      const { address, email } = account;
+      setAccount(address);
+      console.log("account", address, email);
+    } catch (err) {
+      console.log("connect err", err);
     }
-    return <div>
-        <div>{account}</div>
-        <button onClick={()=>connect()}>unipass</button>
+  };
+  const signMsg = async () => {
+    const msg = "hello pwa";
+    try {
+      const options = { isEIP191Prefix: false, onAuthChain: true };
+      const sig = await upWallet.signMessage(msg, options);
+      setSig(sig);
+    } catch (err) {
+      console.log("auth err", err);
+    }
+  };
+  return (
+    <div>
+      <div>{account}</div>
+      {account ? (
+        <button
+          onClick={() => {
+            setAccount("");
+            setSig("");
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={() => connect()}>unipass</button>
+      )}
+
+      {account && (
+        <div>
+          <button onClick={signMsg}>sign</button>
+          {sig && (
+            <div>
+              <p>sig result:</p>
+              <p>{sig}</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
+  );
 }
