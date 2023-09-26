5b6d15a36c81ddb95822ce7aa5e112f910445adf
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-26 11:43:57 +0800

    update joyid

--- src/components/rjoyid.jsx
+++ src/components/rjoyid.jsx
@@ -1,10 +1,45 @@
-import { connect, signMessage } from "@joyid/evm";
-import { useState } from "react";
+import {
+  connect,
+  signMessage,
+  connectWithRedirect,
+  connectCallback,
+} from "@joyid/evm";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+function buildRedirectUrl(action) {
+  const url = new URL(`${window.location.origin}/redirect`);
+  url.searchParams.set("action", action);
+  return url.href;
+}
 
 export default function ConnectJoyid() {
+  const [searchParams] = useSearchParams();
+
   const [isLoading, setIsLoading] = useState(false);
   const [account, setAccount] = useState();
   const [sig, setSig] = useState("");
+
+  useEffect(() => {
+    const redirectHome = () => {
+      let state;
+      try {
+        state = connectCallback();
+        console.log("-------state:", state);
+        if (state?.address) {
+          setAccount(state.address);
+        }
+      } catch (error) {
+        console.error("-------callback:", error);
+      }
+    };
+    redirectHome();
+  }, []);
+
+  const onConnectRedirect = () => {
+    const url = buildRedirectUrl("connect");
+    connectWithRedirect(url);
+  };
 
   const onConenctPopup = async () => {
     setIsLoading(true);
@@ -38,7 +73,7 @@ export default function ConnectJoyid() {
           )}
         </div>
       ) : (
-        <button onClick={onConenctPopup}>Connect Joyid</button>
+        <button onClick={onConnectRedirect}>Connect Joyid</button>
       )}
     </div>
   );
