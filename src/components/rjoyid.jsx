14129c3839de016c0e94b37aad702629aba8de3e
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-26 18:14:55 +0800

    update

--- src/components/rjoyid.jsx
+++ src/components/rjoyid.jsx
@@ -3,9 +3,12 @@ import {
   connectCallback,
   signMessageWithRedirect,
   signMessageCallback,
+  signTransactionWithRedirect,
 } from "@joyid/evm";
 import { useEffect, useState } from "react";
 import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { ethers } from "ethers";
+import ERC20_ABI from "../abi/abi.json";
 
 function buildRedirectUrl(action) {
   const url = new URL(`${window.location.origin}/test_pwa`);
@@ -13,10 +16,19 @@ function buildRedirectUrl(action) {
   return url.href;
 }
 
+const buildERC20Data = (toAddress, amount) => {
+  
+  const iface = new ethers.utils.Interface(ERC20_ABI);
+  const rawData = iface.encodeFunctionData("transfer", [toAddress, amount]);
+  return rawData;
+};
+
+
 export default function ConnectJoyid() {
   const [isLoading, setIsLoading] = useState(false);
   const [account, setAccount] = useState();
   const [sig, setSig] = useState("");
+  const [toAddress, setToAddress] = useState("0xa2f87DC5ec6F659dC7A13c8f12663D251E72B698");
 
   useEffect(() => {
     const redirectHome = () => {
@@ -55,7 +67,13 @@ export default function ConnectJoyid() {
 
   const onConnectRedirect = () => {
     const url = buildRedirectUrl("connect");
-    connectWithRedirect(url);
+    connectWithRedirect(url, {
+      rpcURL: "https://eth.llamarpc.com",
+      network: {
+        chainId: 1,
+        name: "Ethereum Mainnet",
+      },
+    });
   };
 
   const onSignMessageRedirect = () => {
@@ -63,6 +81,16 @@ export default function ConnectJoyid() {
     const url = buildRedirectUrl("sign-message");
     signMessageWithRedirect(url, msg, account, {
       state: msg,
+    });
+  };
+
+  const onSendRedirect = () => {
+    const url = buildRedirectUrl("send-erc20");
+    signTransactionWithRedirect(url, {
+      to: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+      from: account,
+      value: "0",
+      data: buildERC20Data(toAddress, ethers.utils.parseUnits("1", 6)),
     });
   };
 
@@ -90,6 +118,15 @@ export default function ConnectJoyid() {
               </p>
             </div>
           )}
+          <p>
+            <input
+              type="text"
+              placeholder="reciever"
+              value={toAddress}
+              onChange={(e) => setToAddress(e.target.value)}
+            />
+            <button onClick={onSendRedirect}>send</button>
+          </p>
         </div>
       ) : (
         <button onClick={onConnectRedirect}>Connect Joyid</button>
